d0fea94e8525d18cfced22cedd5df413e262c67a
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2019-02-15 16:45:45 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
    menjadikan RGB ke Gray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('src/img_test.jpg')
-cv2.imshow('result', img)
+grs_img = np.copy(img)
+abuAbu = cv2.cvtColor(grs_img, cv2.COLOR_RGB2GRAY)
+cv2.imshow('result', abuAbu)
 cv2.waitKey(0)
