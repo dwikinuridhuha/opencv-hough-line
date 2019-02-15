3e0fda65b9c1d3d2209863c6c81c7945837b7325
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2019-02-15 16:49:46 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com> 3. mengeblurkan gambar

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import numpy as np
 img = cv2.imread('src/img_test.jpg')
 grs_img = np.copy(img)
 abuAbu = cv2.cvtColor(grs_img, cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(abuAbu, (5, 5), 0)
 cv2.imshow('result', abuAbu)
 cv2.waitKey(0)
